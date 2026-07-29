b5fd29a388d473c92d5223d9799bd7cebfb183d2
Daryl Busson <daryljjbb@yahoo.com>
2026-07-29 15:20:04 -0700

    Added PostSql database

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,17 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
