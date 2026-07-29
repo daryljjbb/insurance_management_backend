d3cba38aba70b2afdc7d4a6769ba7d5cfc976454
Daryl Busson <daryljjbb@yahoo.com>
2026-07-29 15:27:13 -0700

    Added PostSql database

--- backend/settings.py
+++ backend/settings.py
@@ -93,12 +93,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
